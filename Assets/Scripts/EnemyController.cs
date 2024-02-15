9f17f18193cd5d9886d66b84d01c212ebda7dc53
SushantaB <sushanta_bhattacharjee2003@yahoo.com>
2024-02-15 20:55:13 +0530

    Adding Patrolling enemy

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEditor;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public GameObject pointA;
+    public GameObject pointB;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Transform currentPoint;
+    public float Speed;
+
+    void Start()
+    {
+        rb=GetComponent<Rigidbody2D>();
+        anim=GetComponent<Animator>();
+        currentPoint=pointB.transform;
+        anim.SetBool("isWalking",true);
+    }
+
+    void Update()
+    {
+        Vector2 point = currentPoint.position - transform.position;
+        if(currentPoint==pointB.transform)
+        {
+            rb.velocity = new Vector2(Speed,0);
+        }
+        else{
+             rb.velocity = new Vector2(-Speed,0);
+        }
+
+        if(Vector2.Distance(transform.position, currentPoint.position)<0.5f && currentPoint==pointB.transform)
+        {
+            Flip();
+            currentPoint=pointA.transform;
+        }
+        if(Vector2.Distance(transform.position, currentPoint.position)<0.5f && currentPoint==pointA.transform)
+        {
+            Flip();
+            currentPoint=pointB.transform;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
+            playerController.KillPlayer();
+        }
+    }
+
+    private void Flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x*=-1;
+        transform.localScale = localScale;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(pointA.transform.position, 0.3f);
+        Gizmos.DrawWireSphere(pointB.transform.position, 0.3f);
+        Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);
+    }
+}
